3720e36ad46568c2938a9742546a8a5c52b5c314
Alexey Stratonov <a.stratonov@simcase.ru>
2015-07-07 14:58:36 +0300

    Отключил журнал css

--- src/sample/App.java
+++ src/sample/App.java
@@ -18,6 +18,7 @@ import sample.models.Filter;
 import sample.parse.Parse;
 import sample.services.AvitoAdsSuperService;
 import sample.trey.MyTrayIcon;
+import sun.util.logging.PlatformLogger;
 
 import java.sql.SQLException;
 
@@ -67,6 +68,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+        com.sun.javafx.Logging.getCSSLogger().setLevel(PlatformLogger.Level.OFF);
         try {
             JDBCClient jdbcClient = new JDBCClient();
             System.out.println(jdbcClient.isAdExistsByUrl("qwe"));
